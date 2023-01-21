ddf8adbc664d3bfa0428b56bd6562d0ceb173bbb
Anton Kozlov <anton-kozlov@yandex-team.ru>
2023-01-21 15:26:29 +0300

    + pagination

--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -141,7 +141,9 @@ REST_FRAMEWORK = {
     'DEFAULT_THROTTLE_RATES': {
         'user': '10000/day',  # Лимит для UserRateThrottle
         'anon': '1000/day',  # Лимт для AnonRateThrottle
-    }
+    },
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
 }
 
 SIMPLE_JWT = {
