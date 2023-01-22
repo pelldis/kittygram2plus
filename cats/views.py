ce0ac07f510b827f2e17afcaf482d29faa63e724
Anton Kozlov <anton-kozlov@yandex-team.ru>
2023-01-22 06:56:27 +0300

    filters,search,ordering

--- cats/views.py
+++ cats/views.py
@@ -1,5 +1,8 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.throttling import AnonRateThrottle
+from rest_framework.pagination import LimitOffsetPagination
+from .paginaition import CatsPagination
 
 from .permissions import OwnerOrReadOnly
 from .models import Achievement, Cat, User
@@ -11,7 +14,15 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
     throttle_classes = (AnonRateThrottle,)
+    pagination_class = None
+    filterset_fields = ('color', 'birth_year', 'achievements__name',
+                        'owner__username')
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('-birth_year',)
+    search_fields = ('name',)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user) 
